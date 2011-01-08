9cc0482e69d33ed9017ffb82e21785c0cef2eeb2
Eric Liang <ericliang@berkeley.edu>
2011-01-08 01:59:18 -0800

    tagging system and cloud visualization

--- tags.py
+++ tags.py
@@ -6,6 +6,7 @@ from config import *
 import earthMine
 import Image, ImageDraw
 import numpy.linalg as linalg
+import colorsys
 import numpy as np
 import math
 
@@ -61,11 +62,8 @@ class TaggedImage:
   def get_frustrum(self):
     return self.db.select_frustrum(self.info['view-location']['lat'], self.info['view-location']['lon'], self.info['view-direction']['yaw'])
 
-  def get_distance(self, tag, pixel):
-    return 0 # XXX
-
-  def get_distances(self, pixels):
-    assert len(pixels) < 500 # earthmine max
+  def get_pixel_locations(self, pixels):
+    assert len(pixels) < 500, len(pixels) # earthmine max
     conn = earthMine.ddObject()
     viewId = self.info['id']
     viewPixels = [earthMine.ddViewPixel(p[0], p[1]) for p in pixels]
@@ -74,14 +72,60 @@ class TaggedImage:
     for pixel, loc in response:
       if loc: # has valid mapping
         locs[(pixel['x'], pixel['y'])] = loc
-    return locs
+    return locs # map of (x,y) to coords
 
-  def get_pixels(self):
-    for x in range(0, self.image.size[0], 25):
-      for y in range(0, self.image.size[1], 35):
+  def save_point_cloud(self, output):
+    buf = []
+    cloud = {}
+    count = 0
+    for pixel in self.get_pixels(1,1):
+      if count % 2000 == 0:
+        INFO('located %d pixels so far' % count)
+      count += 1
+      buf.append(pixel)
+      if len(buf) > 490:
+        dists = self.get_pixel_locations(buf)
+        cloud.update(dists)
+        buf = []
+    np.save(output, cloud)
+
+  def colordist(self, dist, bound=100.0):
+    offset = .33 # green ... red
+    return tuple(map(lambda i: int(i*255), colorsys.hsv_to_rgb(min(1, dist/bound + offset), 1, 1)))
+
+  def visualize_point_cloud(self, cloudfile, output):
+    cloud = np.load(cloudfile).item()
+    img = self.image.copy()
+    pixels = img.load()
+    for x,y in cloud:
+      d = cloud[x,y]
+      lat, lon = d['lat'], d['lon']
+      pixels[x,y] = self.colordist(distance(lat, lon, self.info['view-location']['lat'], self.info['view-location']['lon']))
+    img = Image.blend(img, self.image, 0.5)
+    img.save(output, 'png')
+
+  def get_pixels(self, stepx=25, stepy=35):
+    for x in range(0, self.image.size[0], stepx):
+      for y in range(0, self.image.size[1], stepy):
         yield (x,y)
 
-  def partial_manhattan_distance(self, d1, d2):
+  def refine_point(self, tag, place):
+    pixel = place[1]
+    worst = place[0]
+    buf = []
+    for dx in range(-50,50,6):
+      for dy in range(-50,50,6):
+        buf.append((pixel[0] + dx, pixel[1] + dy))
+    locs = self.get_pixel_locations(buf)
+    for pixel in buf:
+      if pixel not in locs:
+        continue
+      dist = self.tag_distance(locs[pixel], dict(tag))
+      place = min(place, (dist, pixel))
+    INFO("improved dist by %f (%d%%)" % (worst - place[0], 100*(worst-place[0])/worst))
+    return place
+
+  def tag_distance(self, d1, d2):
     x1, y1, z1 = d1['lat'], d1['lon'], d1['alt']
     x2, y2, z2 = d2['lat'], d2['lon'], d2['alt']
     xydist = distance(x1, y1, x2, y2)
@@ -92,16 +136,16 @@ class TaggedImage:
     "Returns collection of (tag, pixel) pairs"
     possible_tags = self.get_frustrum()
     tag_positions = dict([(tag, (999999, None)) for tag in possible_tags])
-    locs = self.get_distances(list(self.get_pixels()))
+    locs = self.get_pixel_locations(list(self.get_pixels()))
     for pixel in self.get_pixels():
       if pixel not in locs:
         continue
       for tag in possible_tags:
-        dist = self.partial_manhattan_distance(locs[pixel], dict(tag))
+        dist = self.tag_distance(locs[pixel], dict(tag))
         tag_positions[tag] = min(tag_positions[tag], (dist, pixel))
     tags = []
     for tag in tag_positions:
-      places = tag_positions[tag]
+      places = self.refine_point(tag, tag_positions[tag])
       if places[1]:
         t = dict(tag)
         t['map_dist'], t['mapped_coord'] = places
@@ -112,16 +156,15 @@ class TaggedImage:
   def draw(self, points, output):
     draw = ImageDraw.Draw(self.image)
     for tag in points:
-      if tag['map_dist'] < 10.0:
-        coord = tag['mapped_coord']
-        offset = -30
-        for key in tag:
-          draw.text((coord[0], coord[1] + offset), str(tag[key]))
-          offset += 10
-        INFO('mapping tag at %f meters error' % tag['map_dist'])
-      else:
-        INFO('throwing away tag %s' % str(tag))
-    self.image.save(output, 'jpeg')
+      coord = tag['mapped_coord']
+      off_y = -30
+      off_x = -30
+      for key in tag:
+        if key == 'map_dist' or type(tag[key]) == str:
+          draw.text((coord[0] + off_x, coord[1] + off_y), str(tag[key]), fill=self.colordist(tag['map_dist'], 10.0))
+          off_y += 10
+      INFO('mapping tag at %f meters error' % tag['map_dist'])
+    self.image.save(output, 'png')
     INFO("saved to %s" % output)
 
 def _test():
@@ -129,7 +172,8 @@ def _test():
   db = TagCollection('testdata/tags.csv')
   img = TaggedImage('testdata/%s.jpg' % name, 'testdata/%s.info' % name, db)
   points = img.get_tag_points()
-  img.draw(points, 'testdata/output.jpg')
+  img.draw(points, 'testdata/output.png')
+#  img.visualize_point_cloud('testdata/cloud.npy', 'testdata/cloud.png')
 
 if __name__ == '__main__':
   _test()
