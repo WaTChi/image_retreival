f8f1444092bc7d877bfc2a8b351d52b9173970cf
Eric Liang <ericliang@berkeley.edu>
2011-04-17 00:49:09 -0700

    stash apply

--- python/querySystem.py
+++ python/querySystem.py
@@ -6,7 +6,7 @@ from context import DEFAULT_CONTEXT
 C = DEFAULT_CONTEXT.copy()
 
 def run():
-    C.QUERY = 'query1-m'
+    C.QUERY = 'query1-a'
     C.params.update({
       'checks': 1024,
       'trees': 1,
@@ -20,11 +20,11 @@ def run():
     C.topnresults = [1,2,3,4,5,6,7,8,9,10]
     C.locator_function = system.skew_location
 #    C.locator_function = system.load_location
-#    C.match_callback = system.dump_combined_matches
+    C.match_callback = system.dump_combined_matches
     C.cacheEnable = 1
     C.tagcompute = 0
     C.ransac_min_filt = 100
-    C.ncells = 1
+    C.ncells = 10
     results, count = system.characterize(C)
 
     for n in C.topnresults:
