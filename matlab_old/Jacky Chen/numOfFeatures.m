1cc2afe310c51dc9c5ea256d3a506b1a6c0cd6c1
Eric Liang <ericliang@berkeley.edu>
2012-04-15 12:05:20 -0700

    delete matlab files

--- matlab_old/Jacky Chen/numOfFeatures.m
+++
@@ -1,6 +0,0 @@
-function [num] = numOfFeatures(fileName)
-
-fid = fopen(fileName, 'r');
-numSift = textscan(fid,'%f',1);
-num = numSift{1};
-fclose(fid);
--- matlab_old/util/svm_setup.m
+++
@@ -1,3 +0,0 @@
-addpath C:\svm_mex601\
-addpath C:\svm_mex601\bin\
-addpath C:\svm_mex601\matlab\
